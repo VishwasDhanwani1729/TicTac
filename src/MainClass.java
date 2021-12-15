21f401e7596cbda71349d3be9143ea827e5aa731
Admin <18it032@charusat.edu.in>
2021-12-15 14:27:44 +0530

    dynamically making tic-tac-toe

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainClass {
@@ -21,21 +22,37 @@ public class MainClass {
             value = i;
         }
     }
-    private OX[][] ox={{OX.E,OX.E,OX.E},{OX.E,OX.E,OX.E},{OX.E,OX.E,OX.E}};
+    private final int boardLength;
+    private final OX[][] ox;
+
+    MainClass(int boardLength){
+        this.boardLength = boardLength;
+        ox= new OX[boardLength][];
+        for(int i=0;i<boardLength;i++) {
+            ox[i] = new OX[boardLength];
+            for (int j = 0; j < boardLength; j++) {
+                ox[i][j] = OX.E;
+            }
+        }
+    }
+
     private String layout(){
-        String temp=" ========== \n";
-        for(int i=0;i<3;i++) {
-            temp = temp + " " + ox[i][0].value + " | " + ox[i][1].value + " | " + ox[i][2].value + " \n";
-            temp += " ========== \n";
+        String temp="";
+        for(int i=0;i<boardLength;i++) {
+            temp=temp+"| ";
+            for(int j=0;j<boardLength;j++)
+                temp = temp+ox[i][j].value + " | ";
+//            temp = temp + " " + ox[i][0].value + " | " + ox[i][1].value + " | " + ox[i][2].value + " \n";
+            temp+=" \n";
         }
         return temp;
     }
     private boolean winning(OX e){
         int r,c,d1,d2;
         r=c=d1=d2=0;
-        for(int i=0;i<3;i++){
+        for(int i=0;i<boardLength;i++){
             r=c=0;
-            for(int j=0;j<3;j++){
+            for(int j=0;j<boardLength;j++){
                 if(ox[i][j] == e){
                    r++;
                 }
@@ -43,13 +60,13 @@ public class MainClass {
                     c++;
                 }
             }
-            if(r==3 || c==3){
+            if(r==boardLength || c==boardLength){
                 return true;
             }
             if(ox[i][i]==e)d1++;
-            if(ox[i][2-i]==e)d2++;
+            if(ox[i][boardLength-1-i]==e)d2++;
         }
-        return d1==3 || d2==3;
+        return d1==boardLength || d2==boardLength;
     }
     private boolean handlingInput(int currentPlayer){
         Scanner scanner = new Scanner(System.in);
@@ -57,7 +74,7 @@ public class MainClass {
         String position = scanner.nextLine();
         int i= Integer.parseInt(position.substring(0,1));
         int j =Integer.parseInt(position.substring(1));
-        if(j<0 || j>2 || i<0 || i>2) return false;
+        if(j<0 || j>boardLength-1 || i<0 || i>boardLength-1) return false;
         if(ox[i][j]!=OX.E || ox[i][j]==OX.values()[currentPlayer]) return false;
         ox[i][j]=OX.values()[currentPlayer];
         System.out.println(layout());
@@ -69,14 +86,14 @@ public class MainClass {
         int currentPlayer=1;
 
         System.out.println("Player-1 [O] & Player-2 [X]\n");
-        System.out.println("Input Range - [0,2]\n");
+        System.out.println("Input Range - [0,"+(boardLength-1)+"]\n");
         System.out.println(layout());
-        while(gameStatus==Status.none && turn<4){
+        while(gameStatus==Status.none && turn<boardLength+1){
             if(!handlingInput(currentPlayer)) continue;
             currentPlayer=3-currentPlayer;
             turn++;
         }
-        while(gameStatus==Status.none && turn<9){
+        while(gameStatus==Status.none && turn<boardLength*boardLength){
             if(!handlingInput(currentPlayer)) continue;
             if(winning(OX.values()[currentPlayer])){
                 gameStatus = Status.values()[currentPlayer];
@@ -91,7 +108,16 @@ public class MainClass {
             System.out.println(gameStatus.value);
         }
     }
+    public static void init_phase(){
+        Scanner scanner=new Scanner(System.in);
+        int boardLength=Integer.MIN_VALUE;
+        while(boardLength<3) {
+            System.out.print("Enter boardlength: ");
+            boardLength = scanner.nextInt();
+        }
+        new MainClass(boardLength).start();
+    }
     public static void main(String[] args) {
-        new MainClass().start();
+        init_phase();
     }
 }
